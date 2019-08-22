7032236059c4952f077e18bb510de0128a78210e
RamTheeru <theertha808@gmail.com>
2019-08-22 17:20:57 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import{Hero} from './hero';
 import {HEROES}from './mock-heroes';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,applyMyStyle, HeroesComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HelloComponent,applyMyStyle, HeroesComponent, HeroDetailComponent, OrdersComponent, OrderComponent, OrderItemsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
